5b7596f71badf051fe4ef235db3cf65f0867ce19
Julio Trecenti <julio.trecenti@gmail.com>
2023-05-16 11:45:14 -0300

    documentacao das bases internas

---
+++ R/utils-data.R
@@ -0,0 +1,20 @@
+#' da_rfb
+"da_rfb"
+
+#' da_rfb_ativo
+"da_rfb_ativo"
+
+#' da_tempo_trf1
+"da_tempo_trf1"
+
+#' da_trf1_cjpg_pequeno
+"da_trf1_cjpg_pequeno"
+
+#' da_trf1_cpopg
+"da_trf1_cpopg"
+
+#' sf_amazon
+"sf_amazon"
+
+#' sinesp
+"sinesp"
